28cf6750dc17221e29fa115283c8c25af68bc78c
Carlos Urquiza <webquiza@gmail.com>
2021-10-26 19:15:20 -0700

    update Startup.cs to map to project objectives

--- Factory/Startup.cs
+++ Factory/Startup.cs
@@ -4,9 +4,9 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using DoctorOffice.Models;
+using Factory.Models;
 
-namespace DoctorOffice
+namespace Factory
 {
   public class Startup
   {
@@ -25,7 +25,7 @@ namespace DoctorOffice
       services.AddMvc();
 
       services.AddEntityFrameworkMySql()
-        .AddDbContext<DoctorOfficeContext>(options => options
+        .AddDbContext<FactoryContext>(options => options
         .UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
     }
 
